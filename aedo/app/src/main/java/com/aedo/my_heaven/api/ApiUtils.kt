e48873349c5f99bf5497f9397d099df6317bc7ee
KKKYW <hopan678@gmail.com>
2022-04-26 13:02:01 +0900

    URI 바뀜

--- aedo/app/src/main/java/com/aedo/my_heaven/api/ApiUtils.kt
+++ aedo/app/src/main/java/com/aedo/my_heaven/api/ApiUtils.kt
@@ -1,10 +1,11 @@
 package com.aedo.my_heaven.api
 
 import com.aedo.my_heaven.util.`object`.Constant
+import com.aedo.my_heaven.util.`object`.Constant.BASE_URL
 import com.aedo.my_heaven.util.`object`.Constant.BASE_URL_TEST
 
 object ApiUtils {
     val apiService: APIService
-    get() = RetrofitClient.getClient(BASE_URL_TEST).create(APIService::class.java)
+    get() = RetrofitClient.getClient(BASE_URL).create(APIService::class.java)
 
 }
